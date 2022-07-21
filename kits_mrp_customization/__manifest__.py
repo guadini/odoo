e4615a481e4f7556647b525e290ee9ed277340a6
Vishal Kapuriya <vishal@keypress.co.in>
2022-07-21 19:20:34 +0530

    add custom mrp customization module

--- kits_mrp_customization/__manifest__.py
+++ kits_mrp_customization/__manifest__.py
@@ -8,35 +8,35 @@
     'website': "https://www.keypress.co.in",
     'category': 'Manufacturing',
     'version': '15.0.0.0',
-    'depends': ['base','product','stock','mrp','mt_wadax','web'],
+    'depends': ['base','product','stock','mrp','web'],
     'data': [
         # Security
-        'security/ir.model.access.csv',
-        # data
-        'data/ir_cron.xml',
-        'data/ir_action_server.xml',
+        # 'security/ir.model.access.csv',
+        # # data
+        # 'data/ir_cron.xml',
+        # 'data/ir_action_server.xml',
         # views
-        'views/stock_move_line_view.xml',
+        # 'views/stock_move_line_view.xml',
         'views/product_product_view.xml',
         'views/product_template_view.xml',
-        'views/stock_warehouse_view.xml',
-        'views/stock_location_route_view.xml',
-        'views/stock_location_orderpoint_view.xml',
-        'views/mrp_production_view.xml',
-        'views/stock_quant_view.xml',
-        'views/kits_reel_allocate_view.xml',
+        # 'views/stock_warehouse_view.xml',
+        # 'views/stock_location_route_view.xml',
+        # 'views/stock_location_orderpoint_view.xml',
+        # 'views/mrp_production_view.xml',
+        # 'views/stock_quant_view.xml',
+        # 'views/kits_reel_allocate_view.xml',
     ],
-    'assets':{
-        'web.assets_backend': [
-            "kits_mrp_customization/static/src/scss/style.scss",
-            "kits_mrp_customization/static/src/js/momenus.js",
-            "kits_mrp_customization/static/src/js/control_panel.js",
-        ],
-        'web.assets_qweb': [
-            'kits_mrp_customization/static/src/xml/control_panel.xml',
-            'kits_mrp_customization/static/src/xml/momenu.xml',
-        ],
-    },
+    # 'assets':{
+    #     'web.assets_backend': [
+    #         "kits_mrp_customization/static/src/scss/style.scss",
+    #         "kits_mrp_customization/static/src/js/momenus.js",
+    #         "kits_mrp_customization/static/src/js/control_panel.js",
+    #     ],
+    #     'web.assets_qweb': [
+    #         'kits_mrp_customization/static/src/xml/control_panel.xml',
+    #         'kits_mrp_customization/static/src/xml/momenu.xml',
+    #     ],
+    # },
     'application': True,
     'license': 'LGPL-3',
 }
--- kits_mrp_customization/models/__init__.py
+++ kits_mrp_customization/models/__init__.py
@@ -1,12 +1,12 @@
 from . import product_product
 from . import product_template
-from . import stock_move_line
-from . import stock_picking
-from . import stock_warehouse
-from . import stock_location_route
-from . import stock_warehouse_orderpoint
-from . import mrp_production
-from . import stock_quant
+# from . import stock_move_line
+# from . import stock_picking
+# from . import stock_warehouse
+# from . import stock_location_route
+# from . import stock_warehouse_orderpoint
+# from . import mrp_production
+# from . import stock_quant
 
-from . import kits_reel_allocate
-from . import kits_reel_allocate_line
+# from . import kits_reel_allocate
+# from . import kits_reel_allocate_line
