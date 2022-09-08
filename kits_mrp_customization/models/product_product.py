e9cdb5f29e7604601374029e95aeff5e83a4ea75
Vishal Kapuriya <vishal@keypress.co.in>
2022-09-08 13:29:54 +0530

    fix variant issue

--- kits_mrp_customization/models/product_product.py
+++ kits_mrp_customization/models/product_product.py
@@ -1,4 +1,4 @@
-from odoo import models,fields,_
+from odoo import models,fields,_,api
 
 
 class product_product(models.Model):
@@ -6,6 +6,15 @@ class product_product(models.Model):
 
     multilocation_available = fields.Boolean('Available At Multiple Location')
     next_auto_lot_number = fields.Integer('Next Auto Lot Number', default=1)
+
+    @api.onchange('is_reel')
+    def _onchange_is_reel(self):
+        for record in self:
+            if record.is_reel:
+                record.detailed_type = 'product'
+                record.tracking = 'lot'
+            else:
+                record.tracking = 'none'    
 
     def onchange_tracking(self):
         if not self.product_tmpl_id.is_reel and any(product.tracking != 'none' and product.qty_available > 0 for product in self):
