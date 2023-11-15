aa560705158a76e191a29eb8cc461dec396da2da
Francis Del Rosario <francislouie.delrosario@gmail.com>
2023-11-15 19:15:18 +0800

    [FIX] serial number, and multiple label in paper

--- product_print_label_wadax/report/product_label_report.py
+++ product_print_label_wadax/report/product_label_report.py
@@ -6,23 +6,35 @@ from odoo.addons.product.report import product_label_report
 
 
 class ReportProductTemplateLabel_2_5x5(models.AbstractModel):
-    _name = 'report.product_print_label_wadax.producttemplatelabel_2_5x5'
+    _name = 'report.product_print_label_wadax.label_2_5x5'
     _description = 'Product Label Report'
 
     def _get_report_values(self, docids, data):
         values = product_label_report._prepare_data(self.env, data)
         # handler of report multiple generator
-        quantity = sum([value for k, value in data.get('quantity_by_product').items()])
-        if quantity <= 100:
-            sleep = 5
-        if quantity <= 250:
-            sleep = 10
-        elif quantity <= 300:
-            sleep = 15
-        elif quantity > 500:
-            sleep = 60
-        else:
-            sleep = 5
-        time.sleep(sleep)
+        # quantity = sum([value for k, value in data.get('quantity_by_product').items()])
+        # if quantity <= 100:
+        #     sleep = 5
+        # if quantity <= 250:
+        #     sleep = 10
+        # elif quantity <= 300:
+        #     sleep = 15
+        # elif quantity > 500:
+        #     sleep = 60
+        # else:
+        #     sleep = 5
+        # time.sleep(sleep)
         # end of handler delay
         return values
+
+class ReportProductTemplateLabel_2_5x5_Multiple(models.AbstractModel):
+    _name = 'report.product_print_label_wadax.label_2_5x5_multiple'
+    _description = 'Product Label Report'
+
+    def _get_report_values(self, docids, data):
+        values = product_label_report._prepare_data(self.env, data)
+        # rows = 9
+        # column = 3
+        values['page_numbers'] = (values.get('page_numbers') - 1) // (9 * 3) + 1
+        return values
+
--- product_print_label_wadax/wizard/product_label_layout.py
+++ product_print_label_wadax/wizard/product_label_layout.py
@@ -11,6 +11,10 @@ class ProductLabelLayout(models.TransientModel):
 
     product_select_ids = fields.Many2many('product.product', 'product_label_layout_product_id_rel', string="Products")
 
+    is_multiple_label_in_paper = fields.Boolean(
+        string='Multiple Label in Paper',
+    )
+
     @api.depends('print_format')
     def _compute_dimensions(self):
         for wizard in self:
@@ -24,14 +28,64 @@ class ProductLabelLayout(models.TransientModel):
 
         if self.print_format == '2.5x5':
             xml_id = 'product_print_label_wadax.report_product_template_label_2_5x5'
-            location_destination = defaultdict(str)
-            if self.picking_quantity in ['picking', 'custom'] and self.move_line_ids:
+            data['is_multiple_label_in_paper'] = self.is_multiple_label_in_paper
+            if self.is_multiple_label_in_paper:
+                xml_id = 'product_print_label_wadax.report_product_template_label_2_5x5_multiple'
+                self.rows = 9
+                self.columns = 2
+            location_destination = defaultdict(list)
+            custom_barcodes = defaultdict(list)
+            uom_unit = self.env.ref('uom.product_uom_categ_unit', raise_if_not_found=False)
+            product_with_lots = []
+            # Picking... Transfer Quantities
+            if self.picking_quantity == 'picking' and self.move_line_ids:
+                move_line_ids = self.move_line_ids
                 if self.product_select_ids:
-                    data['quantity_by_product'] = {k: v for k, v in data.get('quantity_by_product').items() if k in self.product_select_ids.ids}
-                for line in self.move_line_ids:
-                    location_destination[line.product_id.id] += line.location_dest_id.display_name or line.picking_id.location_dest_id.display_name
-                data['location_dest_by_product'] = {p: str(loc) for p, loc in location_destination.items()}
+                    move_line_ids = self.move_line_ids.filtered(lambda l: l.product_id in self.product_select_ids)
+                    if move_line_ids.filtered(lambda l: not l.lot_id or not l.lot_name):
+                        data['quantity_by_product'] = {k: v for k, v in data.get('quantity_by_product').items() if k in self.product_select_ids.ids}
+                for line in move_line_ids:
+                    if line.product_uom_id.category_id == uom_unit:
+                        if (line.lot_id or line.lot_name) and int(line.qty_done):
+                            location_destination[line.product_id.id].append((line.lot_id.name or line.lot_name, line.location_dest_id.display_name))
+                            custom_barcodes[line.product_id.id].append((line.lot_id.name or line.lot_name, int(line.qty_done)))
+                            continue
+                    if not (line.lot_id or line.lot_name) and int(line.qty_done):
+                        location_destination[line.product_id.id].append(("", line.location_dest_id.display_name ))
+                        continue
+                data['custom_barcodes'] = custom_barcodes
+                data['location_dest_by_product'] = location_destination
                 data['location_destination_temp'] = line.picking_id.location_dest_id.display_name
+            # Custom
+            if self.picking_quantity == 'custom' and self.move_line_ids:
+                move_line_ids = self.move_line_ids
+                if self.product_select_ids:
+                    move_line_ids = self.move_line_ids.filtered(lambda l: l.product_id in self.product_select_ids)
+                    if move_line_ids.filtered(lambda l: not l.lot_id or not l.lot_name):
+                        data['quantity_by_product'] = {k: v for k, v in data.get('quantity_by_product').items() if k in self.product_select_ids.ids}
+                for line in move_line_ids:
+                    if line.product_uom_id.category_id == uom_unit:
+                        if (line.lot_id or line.lot_name) and int(line.qty_done):
+                            location_destination[line.product_id.id].append((line.lot_id.name or line.lot_name, line.location_dest_id.display_name))
+                            qty_done = line.qty_done
+                            if line.qty_done >= self.custom_quantity:
+                                qty_done = self.custom_quantity
+                            if line.product_id.id not in product_with_lots:
+                                product_with_lots.append(line.product_id.id)
+                            custom_barcodes[line.product_id.id].append((line.lot_id.name or line.lot_name, int(qty_done)))
+                            continue
+                    if not (line.lot_id or line.lot_name) and int(line.qty_done):
+                        location_destination[line.product_id.id].append(("", line.location_dest_id.display_name ))
+                        continue
+                # clear quantity_by_product
+                if data.get('quantity_by_product') and product_with_lots:
+                    for product in product_with_lots:
+                        data.get('quantity_by_product').pop(product)
+
+                data['custom_barcodes'] = custom_barcodes
+                data['location_dest_by_product'] = location_destination
+                data['location_destination_temp'] = line.picking_id.location_dest_id.display_name
+
             if self.picking_quantity == 'picking' and not self.move_line_ids:
                 if self.product_tmpl_ids:
                     products = self.product_tmpl_ids.ids
